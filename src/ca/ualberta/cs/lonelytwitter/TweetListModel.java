2311a7559a390431857f11cf9478f070146bfd21
Yu Zuo <zuo2@cs.ualberta.ca>
2014-01-14 19:46:16 -0700

    done

--- src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
+++ src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
@@ -1,7 +1,24 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+
 
 public class TweetListModel
 {
+	private ArrayList<LonelyTweetModel> list;
 
+	
+	public ArrayList<LonelyTweetModel> getList()
+	{
+	
+		return list;
+	}
+
+	
+	public void setList(ArrayList<LonelyTweetModel> list)
+	{
+	
+		this.list = list;
+	}
+	
 }
