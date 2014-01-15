537f78404e9d102d73fbaf32c315def32cdf4a06
Yu Zuo <zuo2@cs.ualberta.ca>
2014-01-14 17:47:01 -0700

    the 2nd : the construture overload

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -1,7 +1,63 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
 
 public class lonelyTweetModel
 {
-
+	private String text;
+	private Date timestamp;
+	//---------------------NOTE----------------------
+	//private -only can see it in the current class.
+	//.....    current class + package.
+	//protected - class + + package.
+	//public  -anywhere
+	
+	
+	//right click the .java then click the source then click the setters and getter
+	//have return type
+	public String getText()
+	{
+	
+		return text;
+	}
+	
+	public void setText(String text)
+	{
+	
+		this.text = text;
+		// it pass an valuable which also called text.
+		// "this" is a keyword the difference "String text" and "private String text"
+	}
+	
+	public Date getTimestamp()
+	{
+	
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date timestamp)
+	{
+	
+		this.timestamp = timestamp;
+	}
+	
+	
+	//right click the .java then click the source then click Field
+	//can only have one.
+	public lonelyTweetModel(String text, Date timestamp)
+	{
+		//this(); run itself it can use for the recursion
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+	
+	
+	public lonelyTweetModel(String text)
+	{
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
+	
 }
